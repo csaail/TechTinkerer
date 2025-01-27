079588bf924e9fe2e62d95df48400fa1c9091af0
csaail <saailchavan02@gmail.com>
2025-01-27 18:41:43 +0530

    add header

--- src/components/Pages/Header/Header.js
+++ src/components/Pages/Header/Header.js
@@ -1,51 +1,58 @@
-  import React, { useState } from 'react';
-  import { Sling as Hamburger } from 'hamburger-react';
-  import './Header.css';
+import React, { useState } from "react";
+import { Sling as Hamburger } from "hamburger-react";
+import "./Header.css";
 
-  const Header = () => {
-    const [isOpen, setOpen] = useState(false);
+const Header = () => {
+  const [isOpen, setOpen] = useState(false);
 
-    return (
-      <>
-        <header className="header">
-          <div className="logo"> ATUL</div>
-          <nav>
-            <ul>
-              <li>
-                <div className="menu-container">
-                  <a href="#">MENU</a>
-                  <div className="hamburger">
-                    <Hamburger toggled={isOpen} toggle={setOpen} />
-                  </div>
+  return (
+    <>
+      <header className="header">
+        <div className="logo">ATUL</div>
+        <nav>
+          <ul>
+            <li>
+              <div className="menu-container">
+                {/* Replace the anchor tag with a button for better accessibility */}
+                <button
+                  className="menu-button"
+                  onClick={() => setOpen(!isOpen)}
+                  aria-expanded={isOpen}
+                >
+                  MENU
+                </button>
+                <div className="hamburger">
+                  <Hamburger toggled={isOpen} toggle={setOpen} />
                 </div>
-              </li>
-            </ul>
-          </nav>
-        </header>
-
-        {/* Full screen overlay menu */}
-        {isOpen && (
-          <div className="menu-overlay">
-            <div className="menu-content">
-              {/* Add the CLOSE text before the Hamburger icon */}
-              <div className="close" onClick={() => setOpen(false)}>
-                <span>CLOSE</span>
-                <Hamburger toggled={isOpen} toggle={setOpen} />
               </div>
-              <ul>
-                <li>Home</li>
-                <li>Portfolio</li>
-                <li>IOT Projects </li>
-                <li>3D Printing</li>
-                <li>3D Designing</li>
-                <li>Custom Requirement</li>
-                <li>Contact</li>
-              </ul>
-            </div>
-          </div>
-        )}
-      </>
-    );
-  };
+            </li>
+          </ul>
+        </nav>
+      </header>
 
-  export default Header;
+      {/* Full screen overlay menu */}
+      {isOpen && (
+        <div className="menu-overlay">
+          <div className="menu-content">
+            {/* Add the CLOSE text before the Hamburger icon */}
+            <div className="close" onClick={() => setOpen(false)}>
+              <span>CLOSE</span>
+              <Hamburger toggled={isOpen} toggle={setOpen} />
+            </div>
+            <ul>
+              <li>Home</li>
+              <li>Portfolio</li>
+              <li>IOT Projects</li>
+              <li>3D Printing</li>
+              <li>3D Designing</li>
+              <li>Custom Requirement</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Header;
